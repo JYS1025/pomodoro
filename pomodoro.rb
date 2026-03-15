c72547245bb321b0ab4cba383f5053d632da0ed8
정윤성 <odysseus@MacBook-Air-59.local>
2026-03-15 23:14:11 +0900

    edited pomodoro.rb

--- pomodoro.rb
+++ pomodoro.rb
@@ -3,11 +3,11 @@ cask "pomodoro" do
   sha256 "75541c9711d45067831d4e6953104e5e27b327d036f2d036e242a7be4b966962"
 
   # Replace with your actual GitHub release URL after publishing
-  url "https://github.com/YOUR_USERNAME/pomodoro/releases/download/v#{version}/Pomodoro.app.zip"
+  url "https://github.com/JYS1025/pomodoro/releases/download/v#{version}/Pomodoro.zip"
 
   name "Pomodoro"
   desc "Minimal macOS menu bar Pomodoro timer"
-  homepage "https://github.com/YOUR_USERNAME/pomodoro"
+  homepage "https://github.com/JYS1025/pomodoro"
 
   app "Pomodoro.app"
 
